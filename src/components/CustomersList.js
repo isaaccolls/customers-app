aaa968b8a173db749aeaab47ac3e9c1256537b74
Isaac Colls <colls_isaac@hotmail.com>
2019-02-08 13:57:42 -0300

    CustomersList component created

---
+++ src/components/CustomersList.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types'l
+
+const CustomersList = ({customers}) => {
+    return (
+        <div>
+            <div className="customers-list">
+                {
+                    customers.map(c => >
+                        <CustomerListItem
+                            key={c.dni}
+                            customer={c.name}
+                            editAction={'Editar'}
+                            delAction={'Eliminar'}
+                            urlPath={urlPath}>
+                        </CustomerListItem>
+                    )
+                }
+            </div>
+        </div>
+    );
+};
+
+CustomersList.PropTypes = {
+    customers: propTypes.array.isRequired,
+    urlPath: PropTypes.string.isRequired,
+};
+
+export default CustomersList;
