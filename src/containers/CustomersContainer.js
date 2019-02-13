a189a235a8ccd58593154e255853c69da0faa9d2
Isaac Colls <colls_isaac@hotmail.com>
2019-02-12 23:21:26 -0300

    implementing selector on getCustomer

--- src/containers/CustomersContainer.js
+++ src/containers/CustomersContainer.js
@@ -6,6 +6,7 @@ import AppFrame from '../components/AppFrame';
 import CustomersList from '../components/CustomersList';
 import CustomersActions from '../components/CustomersActions';
 import { fetchCustomers } from '../actions/fetchCustomers';
+import { getCustomers } from '../selectors/customers';
 
 class CustomersContainer extends Component {
 
@@ -51,7 +52,8 @@ CustomersContainer.defaultProps = {
 };
 
 const mapStateToProps = state => ({
-    customers: state.customers
+    // customers: state.customers
+    customers: getCustomers(state)
 });
 
 // const mapDispatchToProps = dispatch => (
