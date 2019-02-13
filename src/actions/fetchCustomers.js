26ecdb30c5efb10aa605de50d89cdc35081cc7d6
Isaac Colls <colls_isaac@hotmail.com>
2019-02-12 23:50:25 -0300

    implementing Middleware redux-promise to fetch customers data

--- src/actions/fetchCustomers.js
+++ src/actions/fetchCustomers.js
@@ -1,23 +1,9 @@
 import { FETCH_CUSTOMERS } from '../constants';
 import { createAction } from 'redux-actions';
 
-const customers = [
-    {
-        "dni" : "420000",
-        "name": "Bob Marley",
-        "age": 37,
-    },
-    {
-        "dni" : "421000",
-        "name": "Kurt Cobain",
-        "age": 46,
-    },
-    {
-        "dni" : "422000",
-        "name": "Janis Joplin",
-        "age": 21,
-    },
-];
+const url = 'http://localhost:3001/customers';
+
+const apiFetchCustomers = () => fetch(url).then(v => v.json());
 
 // export const fetchCustomers = () => ({ type: 'FETCH_CUSTOMERS', payload: null });
-export const fetchCustomers = createAction(FETCH_CUSTOMERS, () => customers);
+export const fetchCustomers = createAction(FETCH_CUSTOMERS, apiFetchCustomers);
