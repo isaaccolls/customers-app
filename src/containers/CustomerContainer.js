997bd40dc4619aba8518e84509612beaf6fe6011
Isaac Colls <colls_isaac@hotmail.com>
2019-02-16 18:35:28 -0300

    dinamic routing and children for CustomerContainer

--- src/containers/CustomerContainer.js
+++ src/containers/CustomerContainer.js
@@ -3,15 +3,23 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import AppFrame from '../components/AppFrame';
 import { getCustomerByDni } from '../selectors/customers';
+import { Route } from 'react-router-dom';
 
 class CustomerContainer extends Component {
 
+    renderBody = () => (
+        <Route path="/customers/:dni/edit" children={
+            ({ match }) =>  (match ? <p>Es edicion</p> : <p>No es edicion</p>)
+        } />
+    )
+
+    // body={<p>Datos del cliente "{this.props.customer.name}"</p>}
     render() {
         return (
             <div>
                 <AppFrame
                     header={`Cliente: ${this.props.dni}`}
-                    body={<p>Datos del cliente "{this.props.customer.name}"</p>}
+                    body={this.renderBody()}
                 >
                 </AppFrame>
             </div>
