fae2dcb9f7caa208baea484780b2ff59de18d35b
Isaac Colls <colls_isaac@hotmail.com>
2019-02-16 18:10:21 -0300

    second param Props on MapStateToProps for CustomerContainer

--- src/containers/CustomerContainer.js
+++ src/containers/CustomerContainer.js
@@ -10,7 +10,7 @@ class CustomerContainer extends Component {
             <div>
                 <AppFrame
                     header={`Cliente: ${this.props.dni}`}
-                    body={<p>Datos del cliente</p>}
+                    body={<p>Datos del cliente "{this.props.customer.name}"</p>}
                 >
                 </AppFrame>
             </div>
@@ -20,6 +20,11 @@ class CustomerContainer extends Component {
 
 CustomerContainer.propTypes = {
     dni: PropTypes.string.isRequired,
+    customer: PropTypes.object.isRequired,
 };
 
-export default connect(null, null)(CustomerContainer);
+const mapStateToProps = (state, props) => ({
+    customer: state.customers.find(c => c.dni === props.dni)
+});
+
+export default connect(mapStateToProps, null)(CustomerContainer);
