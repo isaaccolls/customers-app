9d5e176038e794179aceae0ae4bbedfdcc3719bf
Isaac Colls <colls_isaac@hotmail.com>
2019-02-20 13:44:58 -0300

    implementing Prompt on CustomerEdit

--- src/components/CustomerEdit.js
+++ src/components/CustomerEdit.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { reduxForm, Field } from 'redux-form';
 import { setPropsAsInitial } from '../helpers/setPropsAsInitial';
 import CustomersActions from './CustomersActions';
+import { Prompt } from 'react-router-dom';
 
 // const isRequired = value => (
 //     !value && "Este campo es requerido"
@@ -44,7 +45,7 @@ const toLower = value => value && value.toLowerCase();
 
 const onlyGrow = (value, previousValue, values) => value && previousValue && (value > previousValue ? value : previousValue);
 
-const CustomerEdit = ({ name, dni, age, handleSubmit, submitting, onBack }) => {
+const CustomerEdit = ({ name, dni, age, handleSubmit, submitting, onBack, pristine, submitSucceeded }) => {
     return (
         <div>
             <h2>Ediccion del cliente</h2>
@@ -68,9 +69,12 @@ const CustomerEdit = ({ name, dni, age, handleSubmit, submitting, onBack }) => {
                     parse={toNumber}
                     normalize={onlyGrow}></Field>
                 <CustomersActions>
-                    <button type="submit" disabled={submitting}>Aceptar</button>
-                    <button onClick={onBack}>Cancelar</button>
+                    <button type="submit" disabled={pristine || submitting}>Aceptar</button>
+                    <button type="button" onClick={onBack} disabled={submitting}>Cancelar</button>
                 </CustomersActions>
+                <Prompt
+                    when={!pristine && !submitSucceeded}
+                    message="Se perderan los datos si continua"></Prompt>
             </form>
         </div>
     );
