9ad1907bec96d748d9a1329b4eaca10f2ea09ec4
Isaac Colls <colls_isaac@hotmail.com>
2019-02-20 00:03:20 -0300

    ServerSide validation with SubmissionError

--- src/api/index.js
+++ src/api/index.js
@@ -5,4 +5,10 @@ export const apiPut = (url, id, obj) => () =>
         method: 'PUT',
         body: JSON.stringify(obj),
         headers: new Headers({'Content-type': 'application/json'})
-    }).then(v => v.json());
+    }).then(v => v.json())
+    .then(r => {
+        if (r.error) {
+            return Promise.reject(r.validation);
+        }
+        return r;
+    });
