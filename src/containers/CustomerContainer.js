5311113a457c8381851344c7ec5c5501900de0bc
Isaac Colls <colls_isaac@hotmail.com>
2019-02-27 14:19:36 -0300

    adding delete button

--- src/containers/CustomerContainer.js
+++ src/containers/CustomerContainer.js
@@ -36,16 +36,31 @@ class CustomerContainer extends Component {
         this.props.history.goBack();
     }
 
+    handleOnDelete = () => {
+        console.log("handleOnDelete started");
+    }
+
+    renderCustomerControl = (isEdit, isDelete) => {
+        if (this.props.customer) {
+            const CustomerControl = isEdit ? CustomerEdit : CustomerData;
+            return <CustomerControl {...this.props.customer}
+                onSubmit={this.handleSubmit}
+                onSubmitSuccess={this.handleOnSubmitSuccess}
+                onBack={this.handleOnBack}
+                isDeleteAllow={!!isDelete}
+                onDelete={this.handleOnDelete}
+                />;
+        }
+        return null;
+    }
+
     renderBody = () => (
         <Route path="/customers/:dni/edit" children={
-            ({ match }) =>  {
-                const CustomerControl = match ? CustomerEdit : CustomerData;
-                return <CustomerControl {...this.props.customer}
-                    onSubmit={this.handleSubmit}
-                    onSubmitSuccess={this.handleOnSubmitSuccess}
-                    onBack={this.handleOnBack}
-                    />;
-            }
+            ({ match: isEdit }) => (
+                <Route path="/customers/:dni/delete" children={
+                    ({ match: isDelete }) => (
+                        this.renderCustomerControl(isEdit, isDelete))
+                } /> )
         } />
     )
 
