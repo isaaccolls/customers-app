d8a22f426448bcef4654e2a680f7dba18b10c888
Isaac Colls <colls_isaac@hotmail.com>
2019-02-27 23:33:25 -0300

    some css improvement

--- src/components/CustomerData.js
+++ src/components/CustomerData.js
@@ -7,9 +7,9 @@ const CustomerData = ({ id, name, dni, age, onBack, isDeleteAllow, onDelete }) =
         <div>
             <div className="customer-data">
                 <h2>Datos del cliente</h2>
-                <div><strong>Nombre</strong><i>{name}</i></div>
-                <div><strong>DNI</strong><i>{dni}</i></div>
-                <div><strong>Edad</strong><i>{age}</i></div>
+                <div><strong>Nombre: </strong><i>{name}</i></div>
+                <div><strong>DNI: </strong><i>{dni}</i></div>
+                <div><strong>Edad: </strong><i>{age}</i></div>
             </div>
             <CustomersActions>
                 <button onClick={onBack}>Volver</button>
--- src/components/CustomerListItem.js
+++ src/components/CustomerListItem.js
@@ -4,17 +4,15 @@ import { Link } from 'react-router-dom';
 
 const CustomerListItem = ({ name, editAction, delAction, urlPath, dni }) => {
     return (
-        <div>
-            <div className="customer-list-item">
-                <div className="field">
-                    <Link to={`${urlPath}${dni}`}>{name}</Link>
-                </div>
-                <div className="field">
-                    <Link to={`${urlPath}${dni}/edit`}>{editAction}</Link>
-                </div>
-                <div className="field">
-                    <Link to={`${urlPath}${dni}/delete`}>{delAction}</Link>
-                </div>
+        <div className="customers-list-item">
+            <div className="field">
+                <Link to={`${urlPath}${dni}`}>{name}</Link>
+            </div>
+            <div className="field">
+                <Link to={`${urlPath}${dni}/edit`}>{editAction}</Link>
+            </div>
+            <div className="field">
+                <Link to={`${urlPath}${dni}/delete`}>{delAction}</Link>
             </div>
         </div>
     );
